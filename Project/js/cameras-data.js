6f364397d8c077e2b2793db8e93e82dd0bcc06e6
Khachik Ghazaryan <ghazaryan.xacho@gmail.com>
2017-06-21 23:19:07 +0400

    Dynamic add elements on pages using json

--- Project/js/cameras-data.js
+++ Project/js/cameras-data.js
@@ -1,15 +1,20 @@
+var data = '{"camName":["Camera_1","Camera_2","Camera_3","Camera_4"], \
+            "location":["Location_1","Location_2","Location_3","Location_4"], \
+            "onOff":["Online","Online","Offline","Online"]}';
+
 $(document).ready(function(){
     var text = "";
     var i;
-    for (i = 0; i < 5; i++) {
+    data = JSON.parse(data);
+    for (i = 0; i < data.camName.length; i++) {
     text += ' \
         <div class="col-sm-8 col-sm-offset-2 col-xs-10 col-xs-offset-1 camera-info">\
             <div class="cam-foto">\
             </div>\
             <div class="info" >\
-                <p class="info-field" style="margin: 2% 0  0 ;"><b>Camera ' + (i + 1) + '</b></p>\
-                <p class="info-field">Location</p>\
-                <p class="info-field">Online/Offline</p>\
+                <p class="info-field" style="margin: 2% 0  0 ;"><b> ' + data.camName[i] + '</b></p>\
+                <p class="info-field">' + data.location[i] + '</p>\
+                <p class="info-field">' + data.onOff[i] + '</p>\
             </div>\
             <!--<div class="icons">\
                 <button class="btn glyphicon glyphicon-edit col-md-3 col-xs-3 col-sm-3 col-md-offset-1 col-xs-offset-1 col-sm-offset-1"\ title="Edit">\
@@ -18,5 +23,13 @@ $(document).ready(function(){
             </div>-->\
         </div>';
             }
-    document.getElementById("cam-area").innerHTML = text;
+    document.getElementById("cam-area").innerHTML += text;
 });
+
+
+var searchBy = function() {
+   
+}
+
+
+
