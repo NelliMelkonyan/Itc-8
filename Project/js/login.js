2d596c36eb346f7833de52aaed533ff3877f9788
Nelli <nelli.melkonyan.97@mail.ru>
2017-06-22 01:41:06 +0300

    Added login authorization

--- Project/js/login.js
+++ Project/js/login.js
@@ -1,22 +1,20 @@
 $(document).ready(function(){
     $('#btnLogId').click(function(){
-        alert("Click");
         var mail = document.getElementById('email').value;
         var pass = document.getElementById('password').value;
         if (checkEmail(mail)){
             if (checkPassword(pass)) {
-                //window.parent.document.getElementById("content").src = "resurces/home.html";
-                document.getElementById("content").src = "resurces/home.html";
-                /*
-                 window.parent.document.getElementById('log').innerHTML = "<i class='icon glyphicon glyphicon-user'></i>" + "&nbsp;&nbsp;Admin" + "<i class='icon glyphicon glyphicon-arrow'></i>";
-                 window.parent.document.getElementById('content').src = "resurces/home.html";
-                 location.reload(false);
-                 var exhours = 5;
-                 var d = new Date();
-                 d.setTime(d.getTime() + (exhours * 60 * 60 * 1000));
-                 var expires = "expires=" + d.toUTCString();
-                 document.cookie = mail + "=" + pass + ";" + expires + ";path=/";
-                 */
+                window.parent.document.getElementById('login').id = "logout";
+                window.parent.document.getElementById("content").src = "resurces/home.html";
+                window.parent.document.getElementById('log').innerHTML =
+                    "<i class='icon glyphicon glyphicon-user'></i>" + "&nbsp;&nbsp;Admin"
+                    + "<i class='icon glyphicon glyphicon-arrow'></i>";
+
+                var exhours = 5;
+                var d = new Date();
+                d.setTime(d.getTime() + (exhours * 60 * 60 * 1000));
+                var expires = "expires=" + d.toUTCString();
+                document.cookie = mail + "=" + pass + ";" + expires + ";path=/";
             }
         }
     });
@@ -25,7 +23,6 @@ $(document).ready(function(){
 function checkEmail(email) {
     /*var email = document.getElementById('email');*/
     //var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-    alert("email");
     var filter = /^[a-zA-Z]+[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
     if (!filter.test(email)) {
         alert('Please provide a valid email address');
@@ -39,7 +36,6 @@ function checkPassword(pass) {
     // at least one number, one lowercase and one uppercase letter
     // at least six characters that are letters, numbers or the underscore
     /*var filter = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])\w{6,}$/;*/
-    alert("pass");
     var filter = /(?=.*[a-z])(?=.*[A-Z]).{6,15}/;
     if (!filter.test(pass)) {
         alert('Password must contain at least 6 characters, at least one number, one lowercase and one uppercase letter.All characters are allowed');
@@ -48,4 +44,3 @@ function checkPassword(pass) {
     }
     return true;
 }
-
