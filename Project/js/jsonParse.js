fe71696bcdc50177d0744781d98c13f5f36d625c
Vrezh Unanyan <vrezhunanyan1997@mail.ru>
2017-06-22 12:36:53 +0400

    Change parse javascript with callback function.

--- Project/js/jsonParse.js
+++ Project/js/jsonParse.js
@@ -2,16 +2,52 @@
  * Created by vrezh on 6/21/17.
  */
 
-function jsonParse(file) {
+var cameraName = [];
+
+function jsonParse(file, callback) {
         var xmlhttp = new XMLHttpRequest();
         xmlhttp.onreadystatechange = function() {
-            myObj = JSON.parse(this.responseText);
-            var text = "";
+            if (this.readyState == 4) {
+                myObj = JSON.parse(this.responseText)
+                callback(myObj);
+            }
+        };
+        xmlhttp.open("GET", file, true);
+        xmlhttp.send();
+}
 
-            for (var i = 0; i < myObj.camera.length; ++i) {
-                createSearchByCameraMenu(myObj.camera[i].cameraName);
-                for (var j = 0; j < myObj.camera[i].images.length; ++j) {
-                    text += '\
+var createSearchByCameraMenu = function () {
+    var a = document.getElementById("searchByCamera");
+    for (var i = 0; i <= a.childNodes.length; ++i) {
+        a.removeChild(a.childNodes[i]);
+    }
+
+    for (var i = 0; i < cameraName.length; ++i) {
+        var li = document.createElement("li");
+        var a = document.createElement("a");
+        var aText = document.createTextNode(cameraName[i]);
+        a.appendChild(aText);
+        li.appendChild(a);
+        li.onclick = function () {
+            var name = cameraName[i];
+            cameraName[i] = cameraName[0];
+            cameraName[0] = name;
+        }
+        var ul = document.getElementById("searchByCamera");
+        ul.appendChild(li);
+    }
+}
+
+var createItem = function(myObj) {
+    var text = "";
+    for (var i = 0; i < myObj.camera.length; ++i) {
+        cameraName[i] = myObj.camera[i].cameraName;
+    }
+
+    for (var i = 0; i < myObj.camera.length; ++i) {
+        //createSearchByCameraMenu(myObj.camera[i].cameraName);
+        for (var j = 0; j < myObj.camera[i].images.length; ++j) {
+            text += '\
                     <div class="detected-object row">\
                         <img class="col-md-3 col-xs-3 col-sm-3" src="' + myObj.camera[i].images[j].imageName + '">\
                         <div class="col-xs-9 col-sm-9 col-md-9 ficsed-data">\
@@ -24,35 +60,12 @@ function jsonParse(file) {
                             <button type="button" class="btn glyphicon glyphicon-paperclip col-md-3 col-xs-3 col-sm-3 col-md-offset-1 col-xs-offset-1 col-sm-offset-1"> </button>\
                         </div>-->\
                     </div>';
-                }
-            }
-
-            document.getElementById("detected-objects").innerHTML = text;
-
-        };
-
-        xmlhttp.open("GET", file, true);
-        xmlhttp.send();
-
-}
-
-var createSearchByCameraMenu = function (cameraName) {
-        var li = document.createElement("li");
-        var a = document.createElement("a");
-        var aText = document.createTextNode(cameraName);
-        a.appendChild(aText);
-        li.appendChild(a);
-        li.onclick = function () {
-
         }
-        var ul = document.getElementById("searchByCamera");
-        ul.appendChild(li);
+    }
+    document.getElementById("detected-objects").innerHTML = text;
 }
-
-
 
 $(document).ready(function() {
-   jsonParse("../json/detectedObject.json") ;
-
+   jsonParse("../json/detectedObject.json",createItem) ;
 });
 
